9275e1e19c1cdcb38ec90f3bf9ea3c262fb140df
Yi-Kun Yang <ykyang@gmail.com>
2024-01-12 08:22:06 -0600

    Refine Ch-7 tests

--- persistable_todo_cache/ch_7.ex
+++ persistable_todo_cache/ch_7.ex
@@ -172,4 +172,5 @@ defmodule Ch7 do
 
     :ok
   end
+  ## 7.4 Reasoning with processes
 end
--- persistable_todo_cache/test/todo/cache_test.exs
+++ persistable_todo_cache/test/todo/cache_test.exs
@@ -2,25 +2,45 @@ defmodule Todo.CacheTest do
   use ExUnit.Case
 
   test "server_process" do
-    todc = Todo.Cache
+    tche = Todo.Cache
 
-    {:ok, cache} = todc.start()
-    pid_bob = todc.server_process(cache, "bob")
-    assert pid_bob != todc.server_process(cache, "alice")
-    assert pid_bob == todc.server_process(cache, "bob")
+    {:ok, pid_cache} = tche.start()
+    assert File.exists?("persist/")
+
+    pid_amy = tche.server_process(pid_cache, "amy")
+    pid_bob = tche.server_process(pid_cache, "bob")
+
+    assert pid_bob != pid_amy
+    assert pid_amy == tche.server_process(pid_cache, "amy")
+    assert pid_bob == tche.server_process(pid_cache, "bob")
   end
 
   test "todo operations" do
-    tcah = Todo.Cache
+    File.rm("./persist/bobs_list")
+    File.rm("./persist/amys_list")
+
+    tche = Todo.Cache
     tsrv = Todo.Server
 
-    {:ok, cache} = tcah.start()
+    {:ok, pid_cache} = tche.start()
+    assert File.exists?("persist/")
 
-    alice = tcah.server_process(cache, "alice")
-    tsrv.add_entry(alice, %{date: ~D[2023-12-19], title: "Dentist"})
-
-    entries = tsrv.entries(alice, ~D[2023-12-19])
+    # Bob
+    pid_server = tche.server_process(pid_cache, "bobs_list")
+    tsrv.add_entry(pid_server, %{date: ~D[2023-12-19], title: "Dentist"})
+    entries = tsrv.entries(pid_server, ~D[2023-12-19])
     ## Use pattern match
     assert [%{date: ~D[2023-12-19], title: "Dentist"}] = entries
+
+    # Amy
+    pid_server = tche.server_process(pid_cache, "amys_list")
+    tsrv.add_entry(pid_server, %{date: ~D[2023-12-19], title: "Dentist"})
+    entries = tsrv.entries(pid_server, ~D[2023-12-19])
+    ## Use pattern match
+    assert [%{date: ~D[2023-12-19], title: "Dentist"}] = entries
+  end
+
+  test "Lots of TODO list" do
+    # TODO
   end
 end
